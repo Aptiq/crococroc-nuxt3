e7234cc4b257a4158d8e1476b487c0fa32bed911
Joey <info@sorana.ch>
2024-11-12 23:59:33 +0100

    fix: Ajout de la structure de base pour le composant AppNavigation

---
+++ components/AppNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <UVerticalNavigation :links="navigationLinks" />
+  </div>
+</template>
+
+<script setup>
+const navigationLinks = [
+  { label: 'Tableau de bord', icon: 'i-heroicons-home', to: '/dashboard' },
+  { label: 'Analyses', icon: 'i-heroicons-chart-bar', to: '/analyses' },
+  { label: 'Matières', icon: 'i-heroicons-cube', to: '/materials' }
+]
+</script> 
